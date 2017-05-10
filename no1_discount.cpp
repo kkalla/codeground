a550fe92c84e39c1b21c96fab1281e19c7b48f63
Kkalla <dkswns333@gmail.com>
2017-05-10 17:14:02 +0900

    Update no1_discount.cpp

--- no1_discount.cpp
+++ no1_discount.cpp
@@ -1,22 +1,48 @@
 #include <cstdio>
 #include <iostream>
+#include <climits>
 
 using namespace std;
 
+int minDistance(int dist[],bool sptset[]){
+	int min = INT_MAX, min_index;
+
+	for(int i=0;i<sptset.size();++i){
+		if(sptset[i]==false && dist[i]<=min)
+			min=dist[i],min_index = i;
+	}
+	return min_index
+}
+
+int Dijkstra(bool** graph,int** cost,int s, int e){
+	int size = graph[0].max_size();
+	int dist[size];
+	bool sptset[size];
+
+	for(int i=0; i<size;++i)
+		dist[i]=INT_MAX,sptset[i]=false;
+	dist[s]=0;
+
+	for(int count=0;count<size-1;count++){
+		int u=minDistance(dist,sptset);
+		sptset[u]=true;
+		for(int v=0;v<size;v++){
+			if(!sptset[v] && graph[u][v] && dist[u] != INT_MAX
+					&& dist[u]+cost[u][v]<dist[v])
+				dist[v] = dist[u]+cost[u][v];
+		}
+	}
+
+	return dist[e];
+
+}
+
+
 int main(int argc, char** argv) {
-		/* 아래 freopen 함수는 sample_input.txt 를 read only 형식으로 연 후,
-		 * 앞으로 표준 입력(키보드) 대신 sample_input.txt 파일로 부터 읽어오겠다는 의미의 코드입니다.
-		 * 만약 본인의 PC 에서 테스트 할 때는, 입력값을 sample_input.txt에 저장한 후 freopen 함수를 사용하면,
-		 * 그 아래에서 scanf 함수 또는 cin을 사용하여 표준입력 대신 sample_input.txt 파일로 부터 입력값을 읽어 올 수 있습니다.
-		 * 또한, 본인 PC에서 freopen 함수를 사용하지 않고 표준입력을 사용하여 테스트하셔도 무방합니다.
-		 * 단, Codeground 시스템에서 "제출하기" 할 때에는 반드시 freopen 함수를 지우거나 주석(//) 처리 하셔야 합니다. */
-		//freopen("sample_input.txt", "r", stdin);
-		//
-		//	/* setbuf 함수를 사용하지 않으면, 본인의 프로그램이 제한 시간 초과로 강제 종료 되었을 때,
-		//	C++에서 printf를 사용할 경우, printf로 출력한 내용이 채점되지 않고 '0점'이 될 수도 있습니다.
-		//	따라서 printf를 사용할 경우 setbuf(stdout, NULL) 함수를 반드시 사용하시기 바랍니다. */
-		//	setbuf(stdout, NULL);
-		//
+		
+		freopen("sample_input.txt", "r", stdin);
+		setbuf(stdout, NULL);
+		
 		
 	int T;
 	int test_case;
@@ -26,7 +52,38 @@ int main(int argc, char** argv) {
 		int stations;
 		int num_rails;
 		int discount;
-		cin >> station >> num_rails >> discout;
+		int result =0;
+		int s;
+		int e;
+		int c;
+		//create empty graph
+		bool** graph;
+		int** cost;
+		graph = new boll*[stations];
+		cost = new int*[stations];
+		for(int i=0;i<stations;++i){
+			graph[i]=new bool[stations]={false};
+			cost[i]=new int[stations]={INT_MAX};
+		}
+		cin >> station >> num_rails >> discount;
+		//input initial values to graph and cost
+		for(int i=0;i<num_rails;++i){
+			cin>>s>>e>>c;
+			graph[s][e] = graph[e][s]=true;
+			cost[s][e] = cost[e][s]=c;
+		}
+		int p;
+		cin >> p;
+		for(int i=0;i<p; ++i){
+			cin >> s >> e;
+			if(graph[s][e] && cost[s][e]>discount)
+				result++;
+			else if(graph[s][e]==false){
+				int total_cost = Dijkstra(graph,cost,s,e);
+				if(total_cost>discount)
+					result++;
+			}
+		}
 
 
 
@@ -34,7 +91,7 @@ int main(int argc, char** argv) {
 
 		// 이 부분에서 정답을 출력하십시오. Codeground 시스템에서는 C++에서도 printf 사용을 권장하며, cout을 사용하셔도 됩니다
 		printf("Case #%d\n", test_case);
-		
+		printf("%d\n",result);
 		}
 	return 0;
 }
